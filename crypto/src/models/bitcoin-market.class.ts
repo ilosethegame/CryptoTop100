c44a9436e0dbd0c9bfd2089076d6e655a6144eff
Nicole Harrington <ilosethegame116@gmail.com>
2018-04-01 16:22:57 -0400

    added sort functionality, routing, 404, and bitcoin header to application

---
+++ crypto/src/models/bitcoin-market.class.ts
@@ -0,0 +1,18 @@
+export class BitcoinMarket {
+    public bitcoin_percentage_of_market_cap: number;
+    public active_cryptocurrencies: number;
+    public total_24h_volume_usd: number;
+    public active_markets: number;
+    public total_market_cap_usd: number;
+
+    constructor(data?: any) {
+        const defaults = {
+            ...data
+        };
+        this.bitcoin_percentage_of_market_cap = defaults.bitcoin_percentage_of_market_cap;
+        this.active_cryptocurrencies = defaults.active_cryptocurrencies;
+        this.total_24h_volume_usd = defaults.total_24h_volume_usd;
+        this.active_markets = defaults.active_markets;
+        this.total_market_cap_usd = defaults.total_market_cap_usd;
+    }
+}
